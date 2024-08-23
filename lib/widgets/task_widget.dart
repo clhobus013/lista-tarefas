0d2ce9b3891a81c0cbe2de40cfdaa627e8126397
claudia <claudiah@dakota.com.br>
2024-08-23 13:09:12 -0300

    Cria widget task

---
+++ lib/widgets/task_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TaskWidget extends StatefulWidget {
+  const TaskWidget({super.key});
+
+  @override
+  State<TaskWidget> createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Text("TESTE"),
+        ],
+      ),
+    );
+  }
+
+}
